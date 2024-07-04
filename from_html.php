8154602ee4f4efc68146622146f06ffa605fa4b1
Komang315 <komangsaputra879@gmail.com>
2024-07-04 22:37:51 +0700

    komen

--- from_html.php
+++ from_html.php
@@ -1,45 +1,9 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-	<meta>charsea="</UTF-8>| JagoWebDev.com</title>
+	<meta></UTF-8></title>
 </head>
-		<body> {
-		   font-family:"segoe ui";
-		}
-		h1 {
-			font-size: 25px;
-		}
-		input, select {
-			border: 1px solid #CCCCCC;
-			padding: 7px;
-			font-size: 14px;
-		}
-		input[type="submit"] {
-			padding: 7px 15px;
-			margin-left: 120px;
-			cursor: pointer;
-		}
-		label {
-			width: 120px;
-			display: block;
-			float: left;
-		}
-		.checkbox, .radio {
-			float:none;
-			width: auto;
-		}
-		.row::after {
-			content: "";
-			display: block;
-			clear:both;
-		}
-		.row {
-			margin-bottom: 5px;
-			clear: both;
-		}
-		.options {
-			float:left;
-		}
+		<body> 
 	</style>
 </head>
 <body>
